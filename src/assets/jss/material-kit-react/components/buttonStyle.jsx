5fb4768cf1024b69c7905b48a2a6d01d70a6940b
Jack Leslie <jjdl@st-andrews.ac.uk>
2018-09-23 13:33:51 +0100

    Overwrote wrong button rule

--- src/assets/jss/material-kit-react/components/buttonStyle.jsx
+++ src/assets/jss/material-kit-react/components/buttonStyle.jsx
@@ -35,7 +35,7 @@ const buttonStyle = {
     touchAction: "manipulation",
     cursor: "pointer",
     "&:hover,&:focus": {
-      color: "#354A9F",
+      color: "#FFFFFF",
       backgroundColor: grayColor,
       boxShadow:
         "0 14px 26px -12px rgba(153, 153, 153, 0.42), 0 4px 23px 0px rgba(0, 0, 0, 0.12), 0 8px 10px -5px rgba(153, 153, 153, 0.2)"
